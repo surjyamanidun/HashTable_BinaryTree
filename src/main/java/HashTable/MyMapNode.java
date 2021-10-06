6be178e81764f72d485140b6469b8eb49f992b1e
surjya <surjyamaniduan@gmail.com>
2021-10-07 01:07:02 +0530

    add: frequency of Word in Large paragraph

--- src/main/java/HashTable/MyMapNode.java
+++ src/main/java/HashTable/MyMapNode.java
@@ -1,46 +1,44 @@
 package HashTable;
 
 public class MyMapNode <K, V> {
-    K key;
-    V value;
-    MyMapNode<K, V> next;
 
-    public MyMapNode(K key, V value) {
-        this.key = key;
-        this.value = value;
-        next = null;
-    }
+	    K key;
+	    V value;
+	    MyMapNode<K, V> next;
 
-    public K getKey() {
-        return key;
-    }
+	    public MyMapNode(K key, V value) {
+	        this.key = key;
+	        this.value = value;
+	        next = null;
+	    }
+	    public K getKey() {
+	        return key;
+	    }
 
-    public void setKey(K key) {
-        this.key = key;
-    }
+	    public void setKey(K key) {
+	        this.key = key;
+	    }
 
-    public V getValue() {
-        return value;
-    }
+	    public V getValue() {
+	        return value;
+	    }
 
-    public void setValue(V value) {
-        this.value = value;
-    }
+	    public void setValue(V value) {
+	        this.value = value;
+	    }
 
-    public MyMapNode<K, V> getNext() {
-        return next;
-    }
+	    public MyMapNode<K, V> getNext() {
+	        return next;
+	    }
 
-    public void setNext(MyMapNode<K, V> next) {
-        this.next = next;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder nodeString = new StringBuilder();
-        nodeString.append("Node{" + "Key=").append(key).append(" Value=").append(value).append("}");
-        if(next != null)
-            nodeString.append("->").append(next);
-        return nodeString.toString();
-    }
-}
+	    public void setNext(MyMapNode<K, V> next) {
+	        this.next = next;
+	    }
+	    public String toString() {
+	        StringBuilder nodeString = new StringBuilder();
+	        nodeString.append("Node{" + "Key=").append(key).append(" Value=").append(value).append("}");
+	        if(next != null)
+	            nodeString.append("->").append(next);
+	        return nodeString.toString();
+	    }
+	}
